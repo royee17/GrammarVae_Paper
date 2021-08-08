366b3220f11db927d8bed233fd80e2185980c050
royee17 <royeeg17@gmail.com>
2021-08-09 01:00:19 +0300

    added eq optimization

---
+++ equation_optimization/run_all_simulations.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description='Run Simulations')
+    parser.add_argument('--num_of_simulations', type=int, metavar='N', default=10)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = get_arguments()
+    simulations = args.num_of_simulations
+    curr_dir = os.getcwd()
+    for num in range(2, simulations + 1):
+        print(f'############ running simulation number {num} ######################')
+        os.chdir(curr_dir)
+        simulation_path = f'simulation{num}/grammar/'
+        os.chdir(simulation_path)
+        os.system("python run_bo.py")
+
+    print('Done')
